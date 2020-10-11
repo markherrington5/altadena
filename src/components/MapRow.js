64cdee9f17d70bced94768c7dc226e0696758daf
Mark H <markherrington5@gmail.com>
2020-10-11 17:10:18 -0400

    added "dolly creek station"

---
+++ src/components/MapRow.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Row, Col } from "react-bootstrap";
+import GoogleMap from './GoogleMap.js';
+
+const MapRow = props => (
+    <Row className='info-row row-map'>
+        <Col className='address' md={7}>
+            <div>
+                <h2>Location</h2>
+                <ul>
+                    <li>Dolly Creek Station</li>
+                    <li>2409 Acton Road</li>
+                    <li>#161</li>
+                    <li>Vestavia Hills, AL 35243</li>
+                </ul>
+            </div>
+        </Col>
+        <Col md={5} className='gmap'>
+            <GoogleMap />
+        </Col>
+    </Row>
+)
+
+export default MapRow;
