b703faa7b2de367f9b6e0fa27db7329cbd1a7224
markherrington5 <43765724+markherrington5@users.noreply.github.com>
2021-03-25 10:58:12 -0400

    Update LogoRow.js

--- src/components/LogoRow.js
+++ src/components/LogoRow.js
@@ -8,7 +8,7 @@ const LogoRow = props => (
             <img src={Logo} alt="logo" />
         </Col>
         <Col id='contact-info' md={6}>
-            <p>Want to get in touch?  Email <a href="jason@altadenaeyecare.com">jason@altadenaeyecare.com</a></p>
+            <p>Want to get in touch?  Email <a href="mailto: jason@altadenaeyecare.com">jason@altadenaeyecare.com</a></p>
             <p>Want to talk instead?  Call Jason at <a href="tel:+205-202-9607">205-202-9607</a></p>
         </Col>
     </Row>
